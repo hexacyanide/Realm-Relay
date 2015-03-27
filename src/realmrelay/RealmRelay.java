b9f7dcc85185aeffcc8dbcfc593991c52e982074
Evan Su <evan@hexacyanide.com>
2015-03-26 21:58:35 -0600

    Remove autogenerated and version-specific files.

--- src/realmrelay/RealmRelay.java
+++ src/realmrelay/RealmRelay.java
@@ -23,197 +23,197 @@ import realmrelay.net.ListenSocket;
 import realmrelay.packets.Packet;
 
 public final class RealmRelay {
-	public static final RealmRelay instance = new RealmRelay();
-	
-	// #settings
-	public String listenHost = "localhost";
-	public int listenPort = 2050;
-	
-	public boolean bUseProxy = false;
-	public String proxyHost = "socks4or5.someproxy.net";
-	public int proxyPort = 1080;
-	
-	public String remoteHost = "54.226.214.216";
-	public int remotePort = 2050;
-	
-	public String key0 = "311F80691451C71B09A13A2A6E";
-	public String key1 = "72C5583CAFB6818995CBD74B80";
-	// #settings end
-	
-	private final ListenSocket listenSocket;
-	private final List<User> users = new ArrayList<User>();
-	private final List<User> newUsers = new Vector<User>();
-	private final Map<Integer, InetSocketAddress> gameIdSocketAddressMap = new Hashtable<Integer, InetSocketAddress>();
-	private final Map<String, Object> globalVarMap = new Hashtable<String, Object>();
-	
-	private RealmRelay() {
-		Properties p = new Properties();
-		p.setProperty("listenHost", this.listenHost);
-		p.setProperty("listenPort", String.valueOf(this.listenPort));
-		p.setProperty("bUseProxy", String.valueOf(this.bUseProxy));
-		p.setProperty("proxyHost", this.proxyHost);
-		p.setProperty("proxyPort", String.valueOf(this.proxyPort));
-		p.setProperty("remoteHost", this.remoteHost);
-		p.setProperty("remotePort", String.valueOf(this.remotePort));
-		p.setProperty("key0", this.key0);
-		p.setProperty("key1", this.key1);
-		
-		File file = new File("settings.properties");
-		if (!file.isFile()) {
-			try {
-				OutputStream out = new FileOutputStream(file);
-				p.store(out, null);
-				out.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		p = new Properties(p);
-		try {
-			InputStream in = new FileInputStream(file);
-			p.load(in);
-			in.close();
-			this.listenHost = p.getProperty("listenHost");
-			this.listenPort = Integer.parseInt(p.getProperty("listenPort"));
-			this.bUseProxy = Boolean.parseBoolean(p.getProperty("bUseProxy"));
-			this.proxyHost = p.getProperty("proxyHost");
-			this.proxyPort = Integer.parseInt(p.getProperty("proxyPort"));
-			this.remoteHost = p.getProperty("remoteHost");
-			this.remotePort = Integer.parseInt(p.getProperty("remotePort"));
-			this.key0 = p.getProperty("key0");
-			this.key1 = p.getProperty("key1");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		this.listenSocket = new ListenSocket(this.listenHost, this.listenPort) {
+    public static final RealmRelay instance = new RealmRelay();
 
-			@Override
-			public void socketAccepted(Socket localSocket) {
-				try {
-					User user = new User(localSocket);
-					RealmRelay.instance.newUsers.add(user);
-				} catch (Exception e) {
-					e.printStackTrace();
-					try {
-						localSocket.close();
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-				}
-			}
-			
-		};
-	}
-	
-	/**
-	 * error message
-	 * @param message
-	 */
-	public static void error(String message) {
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		String timestamp = sdf.format(new Date());
-		String raw = timestamp + " " + message;
-		System.err.println(raw);
-	}
-	
-	/**
-	 * echo message
-	 * @param message
-	 */
-	public static void echo(String message) {
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		String timestamp = sdf.format(new Date());
-		String raw = timestamp + " " + message;
-		System.out.println(raw);
-	}
-	
-	public Object getGlobal(String var) {
-		return this.globalVarMap.get(var);
-	}
-	
-	public InetSocketAddress getSocketAddress(int gameId) {
-		InetSocketAddress socketAddress = this.gameIdSocketAddressMap.get(gameId);
-		if (socketAddress == null) {
-			return new InetSocketAddress(this.remoteHost, this.remotePort);
-		}
-		return socketAddress;
-	}
-	
-	public void setGlobal(String var, Object value) {
-		this.globalVarMap.put(var, value);
-	}
-	
-	public void setSocketAddress(int gameId, String host, int port) {
-		InetSocketAddress socketAddress = new InetSocketAddress(host, port);
-		this.gameIdSocketAddressMap.put(gameId, socketAddress);
-	}
-	
-	public static void main(String[] args) {
-		try {
-			XmlParser.parseXMLData();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Packet.init();
-		if (RealmRelay.instance.listenSocket.start()) {
-		    RealmRelay.echo("Realm Relay listener started");
-			while (!RealmRelay.instance.listenSocket.isClosed()) {
-				while (!RealmRelay.instance.newUsers.isEmpty()) {
-					User user = RealmRelay.instance.newUsers.remove(0);
-					RealmRelay.instance.users.add(user);
-					RealmRelay.echo("Connected " + user.localSocket);
-					user.scriptManager.trigger("onEnable");
-				}
-				
-				int cores = Runtime.getRuntime().availableProcessors();
-				Thread[] threads = new Thread[cores];
-				int core = 0;
-				Iterator<User> i = RealmRelay.instance.users.iterator();
-				while (i.hasNext()) {
-					final User user = i.next();
-					if (user.localSocket.isClosed()) {
-						i.remove();
-						continue;
-					}
-					if (threads[core] != null) {
-						try {
-							threads[core].join();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					(threads[core] = new Thread(new Runnable() {
-						
-						@Override
-						public void run() {
-							user.process();
-						}
-						
-					})).start();
-					core = (core + 1) % cores;
-				}
-				for (Thread thread: threads) {
-					if (thread == null) {
-						continue;
-					}
-					try {
-						thread.join();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				Thread.yield();
-			}
-			Iterator<User> i = RealmRelay.instance.users.iterator();
-			while (i.hasNext()) {
-				User user = i.next();
-				user.kick();
-			}
-		} else {
-		    RealmRelay.echo("Realm Relay listener problem. Make sure there are no instances of Realm Relay already running.");
-		}
-	}
+    // #settings
+    public String listenHost = "localhost";
+    public int listenPort = 2050;
 
+    public boolean bUseProxy = false;
+    public String proxyHost = "socks4or5.someproxy.net";
+    public int proxyPort = 1080;
+
+    public String remoteHost = "54.226.214.216";
+    public int remotePort = 2050;
+
+    public String key0 = "311F80691451C71B09A13A2A6E";
+    public String key1 = "72C5583CAFB6818995CBD74B80";
+    // #settings end
+
+    private final ListenSocket listenSocket;
+    private final List<User> users = new ArrayList<User>();
+    private final List<User> newUsers = new Vector<User>();
+    private final Map<Integer, InetSocketAddress> gameIdSocketAddressMap = new Hashtable<Integer, InetSocketAddress>();
+    private final Map<String, Object> globalVarMap = new Hashtable<String, Object>();
+
+    private RealmRelay() {
+        Properties p = new Properties();
+        p.setProperty("listenHost", this.listenHost);
+        p.setProperty("listenPort", String.valueOf(this.listenPort));
+        p.setProperty("bUseProxy", String.valueOf(this.bUseProxy));
+        p.setProperty("proxyHost", this.proxyHost);
+        p.setProperty("proxyPort", String.valueOf(this.proxyPort));
+        p.setProperty("remoteHost", this.remoteHost);
+        p.setProperty("remotePort", String.valueOf(this.remotePort));
+        p.setProperty("key0", this.key0);
+        p.setProperty("key1", this.key1);
+
+        File file = new File("settings.properties");
+        if (!file.isFile()) {
+            try {
+                OutputStream out = new FileOutputStream(file);
+                p.store(out, null);
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        p = new Properties(p);
+        try {
+            InputStream in = new FileInputStream(file);
+            p.load(in);
+            in.close();
+            this.listenHost = p.getProperty("listenHost");
+            this.listenPort = Integer.parseInt(p.getProperty("listenPort"));
+            this.bUseProxy = Boolean.parseBoolean(p.getProperty("bUseProxy"));
+            this.proxyHost = p.getProperty("proxyHost");
+            this.proxyPort = Integer.parseInt(p.getProperty("proxyPort"));
+            this.remoteHost = p.getProperty("remoteHost");
+            this.remotePort = Integer.parseInt(p.getProperty("remotePort"));
+            this.key0 = p.getProperty("key0");
+            this.key1 = p.getProperty("key1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.listenSocket = new ListenSocket(this.listenHost, this.listenPort) {
+            @Override
+            public void socketAccepted(Socket localSocket) {
+                try {
+                    User user = new User(localSocket);
+                    RealmRelay.instance.newUsers.add(user);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    try {
+                        localSocket.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+
+    /**
+     * Prints a timestamp-prefixed error to standard error.
+     *
+     * @param message
+     */
+    public static void error(String message) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("MDT"));
+        String timestamp = sdf.format(new Date());
+        String raw = timestamp + " " + message;
+        System.err.println(raw);
+    }
+
+    /**
+     * Prints a timestamp-prefixed message to standard output.
+     * 
+     * @param message
+     */
+    public static void echo(String message) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("MDT"));
+        String timestamp = sdf.format(new Date());
+        String raw = timestamp + " " + message;
+        System.out.println(raw);
+    }
+
+    public Object getGlobal(String var) {
+        return this.globalVarMap.get(var);
+    }
+
+    public InetSocketAddress getSocketAddress(int gameId) {
+        InetSocketAddress socketAddress = this.gameIdSocketAddressMap.get(gameId);
+        if (socketAddress != null)return socketAddress;
+        return new InetSocketAddress(this.remoteHost, this.remotePort);
+    }
+
+    public void setGlobal(String var, Object value) {
+        this.globalVarMap.put(var, value);
+    }
+
+    public void setSocketAddress(int gameId, String host, int port) {
+        InetSocketAddress socketAddress = new InetSocketAddress(host, port);
+        this.gameIdSocketAddressMap.put(gameId, socketAddress);
+    }
+
+    public static void main(String[] args) {
+        try {
+            XmlParser.parseXMLData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Packet.init();
+
+        if (!RealmRelay.instance.listenSocket.start()) {
+            RealmRelay.error("Failed to start relay server. Is another instance still running?");
+            return;
+        }
+
+        RealmRelay.echo("Realm Relay server started successfully.");
+
+        while (!RealmRelay.instance.listenSocket.isClosed()) {
+            while (!RealmRelay.instance.newUsers.isEmpty()) {
+                User user = RealmRelay.instance.newUsers.remove(0);
+                RealmRelay.instance.users.add(user);
+                RealmRelay.echo("Connected " + user.localSocket);
+                user.scriptManager.trigger("onEnable");
+            }
+
+            int cores = Runtime.getRuntime().availableProcessors();
+            Thread[] threads = new Thread[cores];
+            int core = 0;
+            Iterator<User> i = RealmRelay.instance.users.iterator();
+            while (i.hasNext()) {
+                final User user = i.next();
+                if (user.localSocket.isClosed()) {
+                    i.remove();
+                    continue;
+                }
+                if (threads[core] != null) {
+                    try {
+                        threads[core].join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                (threads[core] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        user.process();
+                    }
+                })).start();
+                core = (core + 1) % cores;
+            }
+            for (Thread thread : threads) {
+                if (thread == null) continue;
+
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Thread.yield();
+        }
+
+        // if the server is no longer listening, kick all connected users
+        Iterator<User> i = RealmRelay.instance.users.iterator();
+        while (i.hasNext()) {
+            User user = i.next();
+            user.kick();
+        }
+    }
 }
--- src/realmrelay/net/ListenSocket.java
+++ src/realmrelay/net/ListenSocket.java
@@ -6,74 +6,71 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 public abstract class ListenSocket implements Runnable {
+    private final SocketAddress endpoint;
+    private ServerSocket serversocket;
+    private Thread thread;
 
-	private final SocketAddress endpoint;
-	private ServerSocket serversocket;
-	private Thread thread;
+    public ListenSocket(int port) {
+        this.endpoint = new InetSocketAddress(port);
+    }
 
-	public ListenSocket(int port) {
-		this.endpoint = new InetSocketAddress(port);
-	}
-	
-	public ListenSocket(String hostname, int port) {
-		this.endpoint = new InetSocketAddress(hostname, port);
-	}
-	
-	public boolean isClosed() {
-		return this.serversocket == null || this.serversocket.isClosed();
-	}
+    public ListenSocket(String hostname, int port) {
+        this.endpoint = new InetSocketAddress(hostname, port);
+    }
 
-	public boolean start() {
-		stop();
-		try {
-			this.serversocket = new ServerSocket();
-			this.serversocket.bind(this.endpoint);
-		} catch (Exception e) {
-			stop();
-			return false;
-		}
-		this.thread = new Thread(this);
-		this.thread.start();
-		return true;
-	}
+    public boolean isClosed() {
+        return this.serversocket == null || this.serversocket.isClosed();
+    }
 
-	public void stop() {
-		this.thread = null;
-		if (this.serversocket != null) {
-			try {
-				this.serversocket.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			this.serversocket = null;
-		}
-	}
+    public boolean start() {
+        this.stop();
 
-	@Override
-	public void run() {
-		while (this.serversocket != null && !this.serversocket.isClosed()) {
-			try {
-				final Socket socket = this.serversocket.accept();
-				new Thread(new Runnable() {
+        try {
+            this.serversocket = new ServerSocket();
+            this.serversocket.bind(this.endpoint);
+        } catch (Exception e) {
+            this.stop();
+            return false;
+        }
 
-					@Override
-					public void run() {
-						socketAccepted(socket);
-					}
-					
-				}).start();
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        this.thread = new Thread(this);
+        this.thread.start();
+        return true;
+    }
 
-	public abstract void socketAccepted(Socket socket);
-	
-	@Override
-	public String toString() {
-		return "ListenSocket[endpoint=" + this.endpoint + "]";
-	}
+    public void stop() {
+        this.thread = null;
+        if (this.serversocket == null) return;
 
+        try {
+            this.serversocket.close();
+            this.serversocket = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (!this.isClosed()) {
+            try {
+                final Socket socket = this.serversocket.accept();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        socketAccepted(socket);
+                    }
+                }).start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public abstract void socketAccepted(Socket socket);
+
+    @Override
+    public String toString() {
+        return "ListenSocket[endpoint=" + this.endpoint + "]";
+    }
 }
